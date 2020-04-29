0218be7cf27532ca86a56a4d8116d0cf371ecfb4
alexHyperportal <alex@dimovo.be>
2020-04-29 23:21:25 +0300

    refactoring [next]

--- http/index.js
+++ http/index.js
@@ -4,22 +4,17 @@ const http = require("http");
 const https = require("https");
 const EventEmitter = require("events");
 const url = require("url");
-
-// example option = { port : 'port', hostname:'hostname'}
-
 class WebServer extends EventEmitter {
   constructor() {
     super();
     this.pathReq = {};
     this.logs = {};
     this.logUp = false;
-    this.namePathReq = '';
     this.index = 0;
     this.next = () => {
-      console.log(this.index, ' ===', this.namePathReq)
-      this.index++
-      this.pathReq[this.namePathReq][this.index].call(this, this.req, this.res, this.next);
-    }
+      this.index++;
+      this.pathReq[this.namePathReq][this.index](this.req, this.res, this.next);
+    };
   }
 
   get listen() {
@@ -46,29 +41,14 @@ class WebServer extends EventEmitter {
 
   initServer(port) {
     if (port) this.listen = port;
-    http
-      .createServer((req, res) => {
-        //test
+    http.createServer((req, res) => {
+        this.req = req;
+        this.res = res;
         this.namePathReq = req.method.toLowerCase() + req.url.slice(1);
-
         if (typeof this.pathReq[this.namePathReq][0] == "function") {
-          this.req = req;
-          this.res = res;
-          if (this.pathReq[this.namePathReq][0].length > 2) {
-            this.pathReq[this.namePathReq][0].call(this, this.req, this.res, this.next);
-          } else {
-            this.pathReq[this.namePathReq][0](req, res);
-          }
+          this.pathReq[this.namePathReq][0](this.req, this.res, this.next);
           if (this.logUp) {
-            this.logs[Date.now()] =
-              "method :" +
-              req.method +
-              ", url: " +
-              req.url +
-              ", statusCode: " +
-              res.statusCode +
-              ", Date: " +
-              new Date();
+            this.logs[Date.now()] = `method: ${req.method}, url: ${req.url}, statusCode: ${res.statusCode}, Date: ${new Date()}` 
           }
         }
       })
@@ -76,43 +56,34 @@ class WebServer extends EventEmitter {
   }
 
   get(path, ...cb) {
-    // Метод GET запитує представлення вказаного ресурсу. Запити, які використовують GET, повинні лише отримувати дані.
     this.pathReq["get" + path || "/"] = cb;
   }
 
   head(path, cb) {
-    // Метод HEAD запитує відповідь, ідентичну запиту GET, але без тіла.
     this.pathReq["head" + path || "/"] = cb;
   }
 
   post(path, cb) {
-    // Метод POST використовується для відправки об'єкта на вказаний ресурс, часто викликаючи зміну стану або побічних ефектів на сервері
     this.pathReq["post" + path || "/"] = cb;
   }
   put(path, cb) {
-    // Метод PUT замінює всі поточні представлення цільового ресурсу на корисне навантаження, що вказане в запиті.
     this.pathReq["put" + path || "/"] = cb;
   }
   delete(path, cb) {
-    // Метод DELETE видаляє вказаний ресурс.
     this.pathReq["delete" + path || "/"] = cb;
   }
   connect(path, cb) {
-    // Метод CONNECT встановлює тунель до сервера, ідентифікованого цільовим ресурсом.
     this.pathReq["connect" + path || "/"] = cb;
   }
   options(path, cb) {
-    // Метод OPTIONS використовується для опису варіантів зв'язку до цільового ресурсу.
     this.pathReq["options" + path || "/"] = cb;
   }
   trace(path, cb) {
-    // Метод TRACE виконує тест зворотного зв'язку по шляху до цільового ресурсу.
     this.pathReq["trace" + path || "/"] = cb;
   }
   patch(path, cb) {
-    // Метод PATCH використовується для застосування часткових модифікацій в ресурсі.
     this.pathReq["patch" + path || "/"] = cb;
   }
 }
 
-module.exports = WebServer
+module.exports = WebServer;
