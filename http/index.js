0fee3b58df2c991ee5f5055e7b850e1def2b9766
alexHyperportal <alex@dimovo.be>
2020-12-06 20:12:28 +0200

    001

--- http/index.js
+++ http/index.js
@@ -35,60 +35,64 @@ class WebServer extends EventEmitter {
   set log(bool) {
     this.logUp = bool;
   }
-  get log() {
-    return this.logs;
+  get body(req) {
+    let body = []
+    return req.on('error', err => console.error(err))
+    .on('data', chunck => body.push(chunck))
+    .on('end', () => Buffer.comcat(body).toString())
   }
 
   initServer(port) {
-    if (port) this.listen = port;
+    if (port) this.listen = port
     http.createServer((req, res) => {
-        this.req = req;
-        this.res = res;
-        this.namePathReq = req.method.toLowerCase() + req.url.slice(1);
+        this.req = req
+        this.res = res
+        this.req.body = this.body
+        this.namePathReq = req.method.toLowerCase() + req.url.slice(1)
         if ( Array.isArray(this.pathReq[this.namePathReq]) && typeof this.pathReq[this.namePathReq][0] == "function") {
-          this.pathReq[this.namePathReq][0](this.req, this.res, this.next);
+          this.pathReq[this.namePathReq][0](this.req, this.res, this.next)
           if (this.logUp) {
             this.logs[Date.now()] = `method: ${req.method}, url: ${req.url}, statusCode: ${res.statusCode}, Date: ${new Date()}`
           }
         } else if (typeof this.pathReq[this.namePathReq] == "function") {
-          this.pathReq[this.namePathReq](this.req, this.res, this.next);
+          this.pathReq[this.namePathReq](this.req, this.res, this.next)
           if (this.logUp) {
             this.logs[Date.now()] = `method: ${req.method}, url: ${req.url}, statusCode: ${res.statusCode}, Date: ${new Date()}`
           }
         }
       })
-      .listen(this.listen);
+      .listen(this.listen)
   }
 
   get(path, ...cb) {
-    this.pathReq["get" + path || "/"] = cb;
+    this.pathReq["get" + path || "/"] = cb
   }
 
   head(path, cb) {
-    this.pathReq["head" + path || "/"] = cb;
+    this.pathReq["head" + path || "/"] = cb
   }
 
   post(path, cb) {
-    this.pathReq["post" + path || "/"] = cb;
+    this.pathReq["post" + path || "/"] = cb
   }
   put(path, cb) {
-    this.pathReq["put" + path || "/"] = cb;
+    this.pathReq["put" + path || "/"] = cb
   }
   delete(path, cb) {
-    this.pathReq["delete" + path || "/"] = cb;
+    this.pathReq["delete" + path || "/"] = cb
   }
   connect(path, cb) {
-    this.pathReq["connect" + path || "/"] = cb;
+    this.pathReq["connect" + path || "/"] = cb
   }
   options(path, cb) {
-    this.pathReq["options" + path || "/"] = cb;
+    this.pathReq["options" + path || "/"] = cb
   }
   trace(path, cb) {
-    this.pathReq["trace" + path || "/"] = cb;
+    this.pathReq["trace" + path || "/"] = cb
   }
   patch(path, cb) {
-    this.pathReq["patch" + path || "/"] = cb;
+    this.pathReq["patch" + path || "/"] = cb
   }
 }
 
-module.exports = WebServer;
+module.exports = WebServer
