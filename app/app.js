3724a7916096ab82f0e60f7d40554c586d92f376
Alexsandrua <alexsanderua@yahoo.com>
2026-01-01 22:50:24 +0200

    request get test

--- app/app.js
+++ app/app.js
@@ -1,9 +1,12 @@
 "use strict"
 
-const WebServer =  require("../").http:
+const redis = require("redis");
+const WebServer =  require("../").http;
 
 let app = new WebServer();
 app.initServer();
+
+
 
 app.options("", (req, res) => {
   var headers = {};
@@ -16,4 +19,25 @@ app.options("", (req, res) => {
   headers["Access-Control-Max-Age"] = "86400";
   res.writeHead(200, headers);
   res.end();
+});
+async function setRedis() {
+
+  const  client = await redis.createClient({
+    url: 'redis://172.18.0.2:6379',
+  }).on('error', (err) => console.error('Redis Client Error', err)).connect();
+  await client.set("key0001", "00010111");
+  const value = await client.get("key0001");
+  client.destroy();
+  console.log('data : ',value);
+  return value;
+
+}
+
+
+
+app.get('test', async (req, res) => {
+ setRedis().then((v) => {
+console.log('data v : ',v);
+});
+  
 });
